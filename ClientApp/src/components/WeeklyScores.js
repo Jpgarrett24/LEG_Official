bebdc25b42c66f5a66a20be577086ef6b21183f3
jpgarrett <jpgarrett24@gmail.com>
2020-09-28 13:15:53 -0700

    added records, username, and teamname to scores navbar. Still needs styling and team photo

--- ClientApp/src/components/WeeklyScores.js
+++ ClientApp/src/components/WeeklyScores.js
@@ -33,7 +33,13 @@ const WeeklyScores = (props) => {
             .catch((err) => console.log(err));
     }, []);
 
-    console.log(rosters);
+    if (scores == false || matchups == false || teams == false || rosters == false) {
+        return (
+            <div className="scoresNav">
+                <h1>This week's scores are loading...</h1>
+            </div>
+        )
+    }
 
     return (
         <div className="scoresNav">
@@ -41,10 +47,21 @@ const WeeklyScores = (props) => {
                 return (
                     <div className="matchup" key={idx}>
                         {matchup.map((team, idx) => {
-                            console.log(team);
+                            let oneTeam = rosters.filter((roster, idx) => {
+                                return (roster.roster_id === team.roster_id)
+                            });
+                            oneTeam = oneTeam[0];
+                            let owner = teams.filter((team, idx) => {
+                                return (team.user_id === oneTeam.owner_id);
+                            })[0];
+                            console.log(owner);
+                            let avatar = `https://sleepercdn.com/avatars/`
                             return (
                                 <div className="matchupTeam" key={idx}>
+                                    <Link to={`/team/${owner.user_id}`}>{owner.metadata.team_name}</Link>
+                                    <span className="matchupUsername">{owner.display_name}</span>
                                     <span className="matchupPoints">{team.points}</span>
+                                    <span className="matchupRecord">{oneTeam.settings.wins} - {oneTeam.settings.losses} - {oneTeam.settings.ties}</span>
                                 </div>
                             )
                         })}
